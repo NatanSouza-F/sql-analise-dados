1c3163f3ab818c44ba22f8ba37852bd0e12a1587
Natan Souza <natanfernandes.150@gmail.com>
2026-01-29 19:52:34 -0300

    Create 06_casos_sem_repasse.sql
    Isolar casos com alto valor investido e repasse zerado

---
+++ sql/06_casos_sem_repasse.sql
@@ -0,0 +1,16 @@
+/*
+Objetivo:
+Isolar casos com alto valor investido e repasse zerado (anomalia),
+para apoio a auditoria e análise de risco.
+*/
+
+SELECT
+    txt_municipio,
+    txt_uf,
+    Valor_Investimento,
+    Valor_Repasse,
+    Data_Carga
+FROM dbo.Investimentos_Limpos
+WHERE Valor_Investimento > 1000000
+  AND Valor_Repasse = 0
+ORDER BY Valor_Investimento DESC;
