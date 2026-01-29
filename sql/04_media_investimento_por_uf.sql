06d4c1bb7cea397527230a5b2cf5baff37c4d966
Natan Souza <natanfernandes.150@gmail.com>
2026-01-29 19:51:29 -0300

    Create 04_media_investimento_por_uf.sql
    Identificar as 10 UFs com maior ticket médio de investimento

---
+++ sql/04_media_investimento_por_uf.sql
@@ -0,0 +1,13 @@
+/*
+Objetivo:
+Identificar as 10 UFs com maior ticket médio de investimento,
+considerando apenas UFs com média acima de R$ 500.000.
+*/
+
+SELECT TOP 10
+    txt_uf,
+    AVG(Valor_Investimento) AS Media_Investimento
+FROM dbo.Investimentos_Limpos
+GROUP BY txt_uf
+HAVING AVG(Valor_Investimento) > 500000
+ORDER BY Media_Investimento DESC;
