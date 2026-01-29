0f3f7fbdf37f76a8b13ace840a1cefc9f5b141a7
Natan Souza <natanfernandes.150@gmail.com>
2026-01-29 19:53:05 -0300

    Create 07_filtros_regionais_e_valores.sql
    Exemplos de filtros clássicos (IN, BETWEEN, LIKE) para consultas operacionais.

---
+++ sql/07_filtros_regionais_e_valores.sql
@@ -0,0 +1,25 @@
+/*
+Objetivo:
+Exemplos de filtros clássicos (IN, BETWEEN, LIKE) para consultas operacionais.
+*/
+
+-- 1) Filtro por UFs específicas (ex: Centro-Oeste)
+SELECT *
+FROM dbo.Investimentos_Limpos
+WHERE txt_uf IN ('DF', 'GO', 'MT', 'MS');
+
+-- 2) Investimentos em uma faixa de valor
+SELECT
+    txt_municipio,
+    txt_uf,
+    Valor_Investimento
+FROM dbo.Investimentos_Limpos
+WHERE Valor_Investimento BETWEEN 100000 AND 500000
+ORDER BY Valor_Investimento DESC;
+
+-- 3) Busca textual por padrão no município
+SELECT
+    txt_municipio,
+    txt_uf
+FROM dbo.Investimentos_Limpos
+WHERE txt_municipio LIKE '%PARÁ%';
